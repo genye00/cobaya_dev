139c861ab727fbf003c977bb0f21e3bc5b7ff3b5
genye00 <ye@lorentz.leidenuniv.nl>
2025-03-19 18:11:42 +0100

    scale cut Planck TTTEEE with ACT DR6

---
+++ cobaya/likelihoods/planck_2018_highl_plik/TTTEEE_actdr6.py
@@ -0,0 +1,9 @@
+from cobaya.likelihoods.base_classes import Planck2018Clik
+
+
+class TTTEEE_actdr6(Planck2018Clik):
+    r"""
+    Scale cut High-$\ell$ temperature (l<1000) and polarization (l<600) \textsc{plik} likelihood of Planck's 2018
+    data release \cite{Aghanim:2019ame}.
+    """
+    pass
